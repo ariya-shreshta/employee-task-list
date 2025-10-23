5695ed0c324a80aff1d5a8f0bb28080a5949fa89
Ariya Vijayakumar <Ariya_Vijaykumar@Dell.com>
2025-10-23 13:51:36 +0530

    Added static UI folder with HTML/CSS/JS

---
+++ ui-static/javascript/scripts.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var contentDiv = document.getElementById('content');
+
+    // Load the content of the current page into the contentDiv
+    fetch('content/' + window.location.pathname.split('/').pop())
+        .then(response => response.text())
+        .then(data => contentDiv.innerHTML = data)
+        .catch(err => console.error('Error loading content:', err));
+});
